753f4f4e72998feb31110f532cf6a4c83cdabaf3
lrc-wc <71730640+lrc-wc@users.noreply.github.com>
2020-12-07 14:26:34 -0600

    Update fox-wolf.js

--- winnebago/lake-level/fox-wolf.js
+++ winnebago/lake-level/fox-wolf.js
@@ -1,6 +1,6 @@
 // ref:  https://plotly.com/javascript/table/
 
-Plotly.d3.csv('fox-test.csv', function(err, rows){
+Plotly.d3.csv('fox-wolf.csv', function(err, rows){
 
   function unpack(rows, key) {
   return rows.map(function(row) { return row[key]; });
