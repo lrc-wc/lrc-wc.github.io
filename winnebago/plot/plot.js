a11cdfd13419335f0765f3f60091ea4acc6e0ba5
lrc-wc <71730640+lrc-wc@users.noreply.github.com>
2020-12-14 18:00:22 -0600

    Add files via upload

--- winnebago/plot/plot.js
+++ winnebago/plot/plot.js
@@ -1,59 +1,30 @@
-// ref:  https://plotly.com/javascript/table/
+function makeplot() {
+  Plotly.d3.csv("https://raw.githubusercontent.com/plotly/datasets/master/2014_apple_stock.csv", function(data){ processData(data) } );
 
-Plotly.d3.csv('flow_hour_today.csv', function(err, rows){
+};
 
-  function unpack(rows, key) {
-  return rows.map(function(row) { return row[key]; });
+function processData(allRows) {
+
+  console.log(allRows);
+  var x = [], y = [], standard_deviation = [];
+
+  for (var i=0; i<allRows.length; i++) {
+    row = allRows[i];
+    x.push( row['AAPL_x'] );
+    y.push( row['AAPL_y'] );
   }
-
-  var headerNames = Plotly.d3.keys(rows[0]);
-
-  var headerValues = [];
-  var cellValues = [];
-  for (i = 0; i < headerNames.length; i++) {
-    headerValue = [headerNames[i]];
-    headerValues[i] = headerValue;
-    cellValue = unpack(rows, headerNames[i]);
-    cellValues[i] = cellValue;
-  }
-
-  // clean date
-  for (i = 0; i < cellValues[1].length; i++) {
-  var dateValue = cellValues[1][i].split(' ')[0]
-  cellValues[1][i] = dateValue
-  }
-
-var Blue='#1f77b4';
-var LightBlue='#A6E7E4';   
-var color1='rgb(235, 193, 238)';
-var color2='rgba(228, 222, 249, 0.65)';
-var data = [{
-  type: 'table',
-//  columnwidth: [150,600,1000,900,600,500,1000,1000,1000],
-//  columnorder: [0,1,2,3,4,5,6,7,8,9],
-  header: {
-    values: headerValues,
-    align: "center",
-    line: {width: 1, color: 'rgb(50, 50, 50)'},
-    fill: {color: [Blue]},
-    font: {family: "Arial", size: 12, color: "white"}
-  },
-  cells: {
-    values: cellValues,
-    align: ["center", "center"],
-    line: {color: "black", width: 1},
-    fill: {color: [Blue,LightBlue,LightBlue,LightBlue,LightBlue,Blue,LightBlue,LightBlue,LightBlue,LightBlue,LightBlue,Blue]},
-    font: {family: "Arial", size: 9, color: ["black"]}
-  }
-}]
-
-var layout = {
-  title: "FOX-WOLF BASIN HYDROLOGIC DATA HOURLY FLOWS",
-   autosize: true,
-  // rangemode: "tozero",
-  //width: 500,
-  height: 1000,
+  console.log( 'X',x, 'Y',y, 'SD',standard_deviation );
+  makePlotly( x, y, standard_deviation );
 }
 
-Plotly.newPlot('table1', data, layout);
-});
+function makePlotly( x, y, standard_deviation ){
+  var plotDiv = document.getElementById("plot");
+  var traces = [{
+    x: x,
+    y: y
+  }];
+
+  Plotly.newPlot('myDiv', traces,
+    {title: 'Plotting CSV data from AJAX call'});
+};
+  makeplot();
