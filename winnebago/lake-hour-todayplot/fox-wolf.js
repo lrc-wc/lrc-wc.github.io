ca3bfbd075628a214adb2375c81618e7d3ff383a
lrc-wc <71730640+lrc-wc@users.noreply.github.com>
2020-12-15 13:33:40 -0600

    Update fox-wolf.js

--- winnebago/lake-hour-todayplot/fox-wolf.js
+++ winnebago/lake-hour-todayplot/fox-wolf.js
@@ -1,60 +1,5 @@
 // ref:  https://plotly.com/javascript/table/
 
-Plotly.d3.csv('lake_hour_today.csv', function(err, rows){
-
-  function unpack(rows, key) {
-  return rows.map(function(row) { return row[key]; });
-  }
-
-  var headerNames = Plotly.d3.keys(rows[0]);
-
-  var headerValues = [];
-  var cellValues = [];
-  for (i = 0; i < headerNames.length; i++) {
-    headerValue = [headerNames[i]];
-    headerValues[i] = headerValue;
-    cellValue = unpack(rows, headerNames[i]);
-    cellValues[i] = cellValue;
-  }
-
-  // clean date
-  for (i = 0; i < cellValues[1].length; i++) {
-  var dateValue = cellValues[1][i].split(' ')[0]
-  cellValues[1][i] = dateValue
-  }
-
-var Blue='#1f77b4';
-var LightBlue='#A6E7E4';   
-var color1='rgb(235, 193, 238)';
-var color2='rgba(228, 222, 249, 0.65)';
-var data = [{
-  type: 'table',
-//  columnwidth: [150,600,1000,900,600,500,1000,1000,1000],
-//  columnorder: [0,1,2,3,4,5,6,7,8,9],
-  header: {
-    values: headerValues,
-    align: "center",
-    line: {width: 1, color: 'rgb(50, 50, 50)'},
-    fill: {color: [Blue]},
-    font: {family: "Arial", size: 12, color: "white"}
-  },
-  cells: {
-    values: cellValues,
-    align: ["center", "center"],
-    line: {color: "black", width: 1},
-    fill: {color: [Blue,LightBlue,LightBlue,LightBlue,LightBlue,Blue,LightBlue,LightBlue,LightBlue,LightBlue,LightBlue,Blue]},
-    font: {family: "Arial", size: 9, color: ["black"]}
-  }
-}]
-
-var layout = {
-  title: "FOX-WOLF BASIN HYDROLOGIC DATA DAILY WATER LEVELS",
-   autosize: true,
-  // rangemode: "tozero",
-  //width: 500,
-  height: 1000,
-}
-
 Plotly.newPlot('table1', data, layout);
 });
 
@@ -70,7 +15,7 @@ var trace1 = {
   name: 'OSHKOSH',
   x: unpack(rows, 'Date'),
   y: unpack(rows, 'OSHKOSH'),
-  line: {color: '#17BECF'}
+  line: {color: 'red'}
 }
 
 var trace2 = {
@@ -79,7 +24,7 @@ var trace2 = {
   name: 'STOCKBRIDGE',
   x: unpack(rows, 'Date'),
   y: unpack(rows, 'STOCKBRIDGE'),
-  line: {color: '#7F7F7F'}
+  line: {color: 'orange'}
 }
 
 var trace3 = {
@@ -97,7 +42,7 @@ var trace4 = {
   name: 'MENASHA',
   x: unpack(rows, 'Date'),
   y: unpack(rows, 'MENASHA'),
-  line: {color: '#7F7F7F'}
+  line: {color: 'green'}
 }
 
 var trace5 = {
@@ -106,7 +51,7 @@ var trace5 = {
   name: 'Average',
   x: unpack(rows, 'Date'),
   y: unpack(rows, 'Average'),
-  line: {color: '#7F7F7F'}
+  line: {color: 'orange'}
 }
 
 var trace6 = {
@@ -115,7 +60,7 @@ var trace6 = {
   name: 'Poygan*',
   x: unpack(rows, 'Date'),
   y: unpack(rows, 'Poygan*'),
-  line: {color: '#7F7F7F'}
+  line: {color: 'purple'}
 }
 
 var data = [trace1,trace2,trace3,trace4,trace5,trace6];
