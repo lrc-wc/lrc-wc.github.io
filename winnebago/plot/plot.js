a2ecb809fcb8e186fbb52f8435f25be2c658a550
lrc-wc <71730640+lrc-wc@users.noreply.github.com>
2020-12-14 22:48:43 -0600

    Update plot.js

--- winnebago/plot/plot.js
+++ winnebago/plot/plot.js
@@ -1,4 +1,4 @@
-Plotly.d3.csv('/finance-charts-apple.csv', function(err, rows){
+Plotly.d3.csv('finance-charts-apple.csv', function(err, rows){
 
   function unpack(rows, key) {
   return rows.map(function(row) { return row[key]; });
