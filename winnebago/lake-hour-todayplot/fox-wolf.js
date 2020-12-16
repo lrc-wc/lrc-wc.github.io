fe1f6bc89c4e9d86de4809373a2f1a9907b550e8
lrc-wc <71730640+lrc-wc@users.noreply.github.com>
2020-12-16 14:07:54 -0600

    Update fox-wolf.js

--- winnebago/lake-hour-todayplot/fox-wolf.js
+++ winnebago/lake-hour-todayplot/fox-wolf.js
@@ -79,8 +79,8 @@ var layout = {
     yaxis: {
     tickmode: "linear", //  If "linear", the placement of the ticks is determined by a starting position `tick0` and a tick step `dtick`
     tick0: 1.5.0,
-    dtick: 4.0,
-    title: 'Oshkosh Datum'
+    dtick: 4.0
+    //title: 'Oshkosh Datum'
     }
 };
 Plotly.newPlot('lakeplot', data, layout);
