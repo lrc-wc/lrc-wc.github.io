a45bcc084c3f234b2cf2df138867eb63d39446c1
lrc-wc <71730640+lrc-wc@users.noreply.github.com>
2020-12-16 17:44:35 -0600

    Update fox-wolf.js

--- winnebago/flow-hour-today/fox-wolf.js
+++ winnebago/flow-hour-today/fox-wolf.js
@@ -27,6 +27,7 @@ var Blue='#1f77b4';
 var LightBlue='#A6E7E4';   
 var color1='rgb(235, 193, 238)';
 var color2='rgba(228, 222, 249, 0.65)';
+var format = d3.format(",")
 var data = [{
   type: 'table',
 //  columnwidth: [150,600,1000,900,600,500,1000,1000,1000],
