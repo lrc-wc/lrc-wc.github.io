757beb91c5f890080907392c2d3647331ea7b24c
lrc-wc <71730640+lrc-wc@users.noreply.github.com>
2020-12-18 10:44:57 -0600

    Update fox-wolf.js

--- winnebago/flow-hour-today/fox-wolf.js
+++ winnebago/flow-hour-today/fox-wolf.js
@@ -42,7 +42,7 @@ var data = [{
     align: ["center", "center"],
     line: {color: "black", width: 1},
     fill: {color: [Blue,LightBlue,LightBlue,LightBlue,LightBlue,Blue,LightBlue,LightBlue,LightBlue,LightBlue,LightBlue,Blue]},
-    font: {family: "Arial", size: 9, color: ["black"]}
+    font: {family: "Arial", size: 12, color: ["black"]}
   }
 }]
 
