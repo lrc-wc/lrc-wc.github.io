fb516ae96b235bf625af60c70459be4481053e75
lrc-wc <71730640+lrc-wc@users.noreply.github.com>
2020-12-18 09:40:32 -0600

    Add files via upload

---
+++ winnebago/lake-hour-prev3dayplot/fox-wolf.js
@@ -0,0 +1,86 @@
+// ref:  https://plotly.com/javascript/table/
+
+Plotly.d3.csv('lake_hour_prev3day.csv', function(err, rows){
+
+  function unpack(rows, key) {
+  return rows.map(function(row) { return row[key]; });
+}
+
+var trace1 = {
+  type: "scatter",
+  mode: "lines",
+  name: 'OSHKOSH',
+  x: unpack(rows, 'Date'),
+  y: unpack(rows, 'OSHKOSH'),
+  line: {color: 'red'}
+}
+
+var trace2 = {
+  type: "scatter",
+  mode: "lines",
+  name: 'STOCKBRIDGE',
+  x: unpack(rows, 'Date'),
+  y: unpack(rows, 'STOCKBRIDGE'),
+  line: {color: 'orange'}
+}
+
+var trace3 = {
+  type: "scatter",
+  mode: "lines",
+  name: 'FOND DU LAC',
+  x: unpack(rows, 'Date'),
+  y: unpack(rows, 'FOND DU LAC'),
+  line: {color: 'blue'}
+}
+
+var trace4 = {
+  type: "scatter",
+  mode: "lines",
+  name: 'MENASHA',
+  x: unpack(rows, 'Date'),
+  y: unpack(rows, 'MENASHA'),
+  line: {color: 'green'}
+}
+
+var trace5 = {
+  type: "scatter",
+  mode: "lines",
+  name: 'Average',
+  x: unpack(rows, 'Date'),
+  y: unpack(rows, 'Average'),
+  line: {color: 'black'}
+}
+
+var trace6 = {
+  type: "scatter",
+  mode: "lines",
+  name: 'POYGAN*',
+  x: unpack(rows, 'Date'),
+  y: unpack(rows, 'POYGAN*'),
+  line: {color: 'purple'}
+}
+
+var data = [trace1,trace2,trace3,trace4,trace5,trace6];
+  
+var layout = {
+  title: 'Lake Winnebago Water Levels',
+    xaxis: {
+    autotick: false,
+    ticks: 'outside',
+    tick0: 0.0,
+    dtick: 12.0,
+    ticklen: 6,
+    tickwidth: 2
+    },
+    yaxis: {
+    autotick: false,
+    ticks: 'outside',
+    tick0: 1.5,
+    dtick: 0.5,
+    ticklen: 6,
+    tickwidth: 2,
+    title: 'Oshkosh Datum (ft)'
+    }
+};
+Plotly.newPlot('lakeplot', data, layout);
+})
